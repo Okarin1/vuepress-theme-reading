e4d826c3fc89ca0b5a20766492a94360638f3fab
Okarin1 <Okarin1024@gmail.com>
2022-07-14 15:32:53 +0800

    feat: add night mode and controller

---
+++ components/ControlItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="readerControls-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+.readerControls-item
+  cursor pointer
+  background-color #fff
+  box-shadow 0 8px 20px 0px rgba(146, 143, 143, 0.3)
+  width 48px
+  height 48px
+  transition background-color .2s ease-in-out
+  border-radius 24px
+  line-height 48px
+  text-align center
+  position relative;
+  opacity 0.7
+  &:hover
+    opacity 1
+.readerControls-item:not(:first-child)
+  margin-top 24px
+</style>
---
+++ components/ReaderControls.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="readerControls">
+      <control-item @click.native="backToHome()">
+        <button class="icon home"></button>
+      </control-item>
+      <control-item @click.native="showSide()">
+        <button class="icon catalog"></button>
+      </control-item>
+      <control-item @click.native="themeSwitch()">
+        <button class="icon dark"></button>
+      </control-item>
+      <control-item @click.native="backToTop()">
+        <button class="icon top"></button>
+      </control-item>
+    </div>
+    <side-bar v-show="isShow"></side-bar>
+  </div>
+</template>
+
+<script>
+import ControlItem from "./ControlItem.vue";
+import SideBar from "./SideBar.vue";
+export default {
+  components: { ControlItem, SideBar },
+  name: "ThemeSwitcher",
+  data() {
+    return {
+      isLight: true,
+      isShow: false,
+    };
+  },
+
+  methods: {
+    themeSwitch() {
+      this.isLight = !this.isLight;
+      let htmlTag = document.getElementsByTagName("html")[0];
+      if (this.isLight) {
+        window.localStorage.setItem("theme", "light");
+        htmlTag.setAttribute("data-theme", "light");
+      } else {
+        window.localStorage.setItem("theme", "dark");
+        htmlTag.setAttribute("data-theme", "dark");
+        this.$emit("themeMode", "dark");
+      }
+    },
+    showSide() {
+      this.isShow = !this.isShow;
+    },
+    backToHome() {
+      this.$router.push("/");
+    },
+    backToTop() {
+      window.scrollTo(0, 0);
+    },
+  },
+  mounted() {
+    let mode = window.localStorage.getItem("theme");
+    if (mode == "dark") {
+      this.isLight = false;
+    }
+  },
+};
+</script>
+
+<style lang="stylus" scope>
+.readerControls
+  transition transform 0.25s ease-in-out 0s, opacity 0.25s ease-in-out 0s
+  transform translateY(0px)
+  opacity 1
+  width 18vw
+  overflow visible
+  position fixed
+  bottom 2rem
+  right 0
+  list-style none
+  font-size 12px
+  display flex
+  flex-direction column
+  align-items flex-start
+  @media screen and (max-width: 1300px)
+    width 10vw
+  @media screen and (max-width: 1024px)
+    display none
+  @media only screen and (max-width: 768px)
+    display none
+
+  button
+    background none
+    border 0
+    padding 0
+    text-decoration none
+    cursor pointer
+  .icon
+    width 24px
+    height 24px
+    background-size 100%
+    position absolute
+    left 50%
+    top 50%
+    margin-top -12px
+    margin-left -12px
+    transition background-image .2s ease-in-out
+  .home
+    background url(https://s1.ax1x.com/2022/06/22/jpK6pt.png) no-repeat
+  .catalog
+    background url(https://s1.ax1x.com/2022/06/22/jpMyE4.png) no-repeat
+  .dark
+    background url(https://s1.ax1x.com/2022/06/22/jpMXxP.png) no-repeat
+  .top
+    background url(https://s1.ax1x.com/2022/06/22/jpQlGR.png) no-repeat
+</style>
