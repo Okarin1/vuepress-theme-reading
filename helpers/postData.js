84e3d2a68b3713f446fbc233cd6b2d0edcc29a93
Okarin1 <Okarin1024@gmail.com>
2022-07-14 15:36:59 +0800

    docs: add some comment

--- helpers/postData.js
+++ helpers/postData.js
@@ -1,8 +1,8 @@
-import { compareDate,formatDate } from "./utils"
+import { compareDate, formatDate } from "./utils"
 
 // 过滤博客数据
 export function filterPosts(posts) {
-  posts = posts.filter(item => item.path !== "/")
+  posts = posts.filter((item) => item.path !== "/")
   return posts
 }
 
@@ -13,27 +13,26 @@ export function sortPostsByDate(posts) {
   })
 }
 
-//分类博客数据
-
-export function categoryPostsByDate(postList,fmt,noDate) {
+// 分类博客数据
+export function categoryPostsByDate(postList, fmt, noDate) {
   let dateArr = []
   const sortList = []
   postList.forEach((res, i) => {
     let postTime = postList[i].frontmatter.date
     let formatPostTime = formatDate(postTime, fmt)
     if (postTime) {
-        //Order by fmt
-      categoryPosts(postList,dateArr,sortList,formatPostTime,i)
+      //Order by fmt
+      categoryPosts(postList, dateArr, sortList, formatPostTime, i)
     } else {
       //Other No Order
-      categoryPosts(postList,dateArr,sortList,noDate,i)
+      categoryPosts(postList, dateArr, sortList, noDate, i)
     }
   })
   return sortList
 }
 
-
-export function categoryPosts(postList,dateArr,sortList,category,i){
+// 分类函数
+export function categoryPosts(postList, dateArr, sortList, category, i) {
   if (dateArr.indexOf(category) === -1) {
     sortList.push({
       date: category,
--- helpers/utils.js
+++ helpers/utils.js
@@ -1,40 +1,40 @@
-//时间格式化
-export function formatDate(time, fmt = 'yyyy-MM-dd hh:mm:ss') {
+// 时间格式化
+export function formatDate(time, fmt = "yyyy-MM-dd hh:mm:ss") {
   const date = new Date(time)
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length))
   }
   let o = {
-    'M+': date.getMonth() + 1,
-    'd+': date.getDate(),
-    'h+': date.getHours(),
-    'm+': date.getMinutes(),
-    's+': date.getSeconds()
-  };
+    "M+": date.getMonth() + 1,
+    "d+": date.getDate(),
+    "h+": date.getHours(),
+    "m+": date.getMinutes(),
+    "s+": date.getSeconds(),
+  }
   for (let k in o) {
     if (new RegExp(`(${k})`).test(fmt)) {
-      let str = o[k] + '';
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+      let str = o[k] + ""
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? str : padLeftZero(str))
     }
   }
-  return fmt;
-};
+  return fmt
+}
 
-function padLeftZero (str) {
-  return ('00' + str).substr(str.length);
-};
-
+// 添"0"操作
+function padLeftZero(str) {
+  return ("00" + str).substr(str.length)
+}
 
 // 比对时间
-export function compareDate (a, b) {
-    const aDateNum = getTimeNum(a.frontmatter.date)
-    const bDateNum = getTimeNum(b.frontmatter.date)
-    if (aDateNum === 0 && bDateNum === 0) return 0
-    return bDateNum - aDateNum
-  }
+export function compareDate(a, b) {
+  const aDateNum = getTimeNum(a.frontmatter.date)
+  const bDateNum = getTimeNum(b.frontmatter.date)
+  if (aDateNum === 0 && bDateNum === 0) return 0
+  return bDateNum - aDateNum
+}
 
 // 获取时间的数字类型
-export function getTimeNum (date) {
+export function getTimeNum(date) {
   const dateNum = !date ? 0 : new Date(date).getTime()
   return dateNum
 }
